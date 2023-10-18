ed6ab2ae8d7f45b0d4c54a98c9f4623cadb61a30
laismullerrr <lais.muller@hotmail.com>
2023-10-18 17:06:16 -0300

    feat: estrutura inicial da lista de produtos

--- src/components/ListaProdutos/ListaProdutos.jsx
+++ src/components/ListaProdutos/ListaProdutos.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Styles from "./listaProdutos.styles.css";
 
 const ListaProdutos = () => {
   return (
-    <div>ListaProdutos</div>
-  )
-}
+    <div className="container-lista-produtos">
+      <div className="container">
+        <h2>Alguns títulos que podem te interessar: </h2>
+        <div className="container-lista">
+          <div className="card-livro">
+            <div className="container-imagem-livro">
+              <img src="" alt="capa do livro"></img>
 
-export default ListaProdutos
+            </div>
+            <div className="container-detalhes-livro"></div>
+              <h3>Título do Livro</h3>
+              <p> Autor do Livro</p>
+              <p>Preço</p>
+              <a href="">Botão?</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListaProdutos;
