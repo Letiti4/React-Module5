5989a274fee517dbaf6411ee0127e463adcae792
laismullerrr <lais.muller@hotmail.com>
2023-10-18 19:22:43 -0300

    feat: adição de imagens e estilização provisórias

--- src/components/ListaProdutos/ListaProdutos.jsx
+++ src/components/ListaProdutos/ListaProdutos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Styles from "./listaProdutos.styles.css";
+import CapaLivro from '../../images/livro-capa-teste.png'
 
 const ListaProdutos = () => {
   return (
@@ -9,15 +10,16 @@ const ListaProdutos = () => {
         <div className="container-lista">
           <div className="card-livro">
             <div className="container-imagem-livro">
-              <img src="" alt="capa do livro"></img>
+              <img className="imagem-livro" src={CapaLivro} alt="capa do livro"></img>
 
             </div>
             <div className="container-detalhes-livro"></div>
-              <h3>Título do Livro</h3>
-              <p> Autor do Livro</p>
-              <p>Preço</p>
-              <a href="">Botão?</a>
+              <h3 className="titulo-livro">Título do Livro</h3>
+              <p className="nome-autor">Autor do Livro</p>
+              <p className="preco">R$39,90</p>
+              <a className="botao-compra" href="">Add carrinho</a>
           </div>
+          
         </div>
       </div>
     </div>
