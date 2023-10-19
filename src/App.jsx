2f8994e1f4952b79988b0a1b5829cd492dc41f4d
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 16:58:03 -0300

    refactor: Removendo comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-// import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
