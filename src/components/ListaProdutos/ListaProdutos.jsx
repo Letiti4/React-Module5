439f9bcab971a47445aea16cb03c66b8e5240910
laismullerrr <lais.muller@hotmail.com>
2023-10-19 10:45:50 -0300

    feat: add componente CardLivro

--- src/components/ListaProdutos/ListaProdutos.jsx
+++ src/components/ListaProdutos/ListaProdutos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Styles from "./listaProdutos.styles.css";
-import CapaLivro from '../../images/livro-capa-teste.png'
+import CardLivro from "../CardLivro/CardLivro";
 
 const ListaProdutos = () => {
   return (
@@ -8,18 +8,10 @@ const ListaProdutos = () => {
       <div className="container">
         <h2>Alguns títulos que podem te interessar: </h2>
         <div className="container-lista">
-          <div className="card-livro">
-            <div className="container-imagem-livro">
-              <img className="imagem-livro" src={CapaLivro} alt="capa do livro"></img>
-
-            </div>
-            <div className="container-detalhes-livro"></div>
-              <h3 className="titulo-livro">Título do Livro</h3>
-              <p className="nome-autor">Autor do Livro</p>
-              <p className="preco">R$39,90</p>
-              <a className="botao-compra" href="">Add carrinho</a>
-          </div>
-          
+          <CardLivro/>
+          <CardLivro/>
+          <CardLivro/>
+          <CardLivro/>
         </div>
       </div>
     </div>
