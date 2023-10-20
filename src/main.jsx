e6fbbff839c2f9418011e1b7174781f904dea748
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 22:58:43 -0300

    refactor: Removendo a página app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Rotas from './Rotas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <Rotas />
   </React.StrictMode>,
 )
