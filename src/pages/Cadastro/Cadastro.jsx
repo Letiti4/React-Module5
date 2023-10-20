aaf82c1af04c5f09cde88f38d9d9cc035a031f33
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-10-19 21:30:46 -0300

    feat: criado pagina de cadastro

---
+++ src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Cadastro.styles.css'
+import livrosLc from "../../assets/livrosLc.jpg"
+const Cadastro = () => {
+    return (
+        <section className="containerCadastro">
+            <div className="imagemCadastro">
+                <img src={livrosLc} alt="" />
+            </div>
+            <div className="containerConteudo">
+                <div className="containerGeral">
+                    <div className="containerForm">
+                        <h2>Cadastre-se</h2>
+                        <p>Crie uma conta!</p>
+                        <form>
+                            <div className="formGroup">
+                                <label htmlFor="">Usuário</label>
+                                <input type="usuario" className="formInput" />
+                            </div>
+                            <div className="formGroup">
+                                <label htmlFor="">Email</label>
+                                <input type="email" className="formInput" />
+                            </div>
+                            <div className="formGroup">
+                                <label htmlFor="">Senha</label>
+                                <input type="senha" className="formInput" />
+                            </div>
+                            <div className="formGroup">
+                                <button className="formEnviar">Criar conta</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Cadastro
