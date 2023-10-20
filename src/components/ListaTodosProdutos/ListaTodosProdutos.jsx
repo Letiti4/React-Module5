5320aae25e593d06e204af4fddd8dc8510aec02e
laismullerrr <lais.muller@hotmail.com>
2023-10-19 21:49:39 -0300

    feat: estrutura inicial da lista de todos produtos

--- src/components/ListaTodosProdutos/ListaTodosProdutos.jsx
+++ src/components/ListaTodosProdutos/ListaTodosProdutos.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import CardLivro from '../CardLivro/CardLivro'
+import Style from './listaTodosProdutos.styles.css'
 
 const ListaTodosProdutos = () => {
   return (
-    <div>ListaTodosProdutos</div>
+    <section className='container-lista-todos-produtos'>
+        <div className='container'>
+            <div className='container-grid'>
+
+                <div className='produto-grid'>
+                    <CardLivro/>
+                </div>
+
+                <div className='produto-grid'>
+                    <CardLivro/>
+                </div>
+
+                <div className='produto-grid'>
+                    <CardLivro/>
+                </div>
+
+                <div className='produto-grid'>
+                    <CardLivro/>
+                </div>
+
+                <div className='produto-grid'>
+                    <CardLivro/>
+                </div>
+
+                <div className='produto-grid'>
+                    <CardLivro/>
+                </div>
+            </div>
+        </div>
+
+    </section>
   )
 }
 
