8ef302618bf672a89cd375bbcb6d99906ef79288
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 23:00:48 -0300

    feat: Criação da lógica de layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import '../Layout/layout.style.css'
+
+
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+    return (
+        <section className='layoutContainer'>
+            <div className='layoutContent'>
+                <h1>Os melhores livros disponíveis</h1>
+                <p>Compre livros de qualidade com preços baixos</p>
+            </div>
+
+        </section>
+    )
 }
 
 export default Layout
