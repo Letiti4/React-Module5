a9f1ae6a32c2517f09527410043014ff37e42228
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-10-20 16:07:30 -0300

    fix: alteração na classe da div

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,8 +3,8 @@ import '../Layout/layout.style.css'
 
 const Layout = () => {
     return (
-        <section className='layoutContainer'>
-            <div className='layoutContent'>
+        <section className="layoutContainer">
+            <div className="layoutConteudo">
                 <h1>Os melhores livros disponíveis</h1>
                 <p>Compre livros de qualidade com preços baixos</p>
             </div>
