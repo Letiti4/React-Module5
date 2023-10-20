ebbafb8c7ad997053e6687b03fcf7f01c923499b
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 23:02:37 -0300

    feat: Importação do header

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+// import React from 'react'
 import livrosLc from "../../assets/livrosLc.jpg"
+import Header from "../../components/Header/Header"
+
 const Login = () => {
     return (
-        <section className="containerCadastro">
-            <div className="imagemCadastro">
-                <img src={livrosLc} alt="" />
-            </div>
-            <div className="containerConteudo">
-                <div className="containerGeral">
-                    <div className="containerForm">
-                        <h2>Login</h2>
-                        <form>
-                            <div className="formGroup">
-                                <label htmlFor="">Email</label>
-                                <input type="email" className="formInput" />
-                            </div>
-                            <div className="formGroup">
-                                <label htmlFor="">Senha</label>
-                                <input type="senha" className="formInput" />
-                            </div>
-                            <div className="formGroup">
-                                <button className="formEnviar">Entrar</button>
-                            </div>
-                        </form>
+        <>
+            <Header />
+            <section className="containerCadastro">
+                <div className="imagemCadastro">
+                    <img src={livrosLc} alt="" />
+                </div>
+                <div className="containerConteudo">
+                    <div className="containerGeral">
+                        <div className="containerForm">
+                            <h2>Login</h2>
+                            <form>
+                                <div className="formGroup">
+                                    <label htmlFor="">Email</label>
+                                    <input type="email" className="formInput" />
+                                </div>
+                                <div className="formGroup">
+                                    <label htmlFor="">Senha</label>
+                                    <input type="senha" className="formInput" />
+                                </div>
+                                <div className="formGroup">
+                                    <button className="formEnviar">Entrar</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
