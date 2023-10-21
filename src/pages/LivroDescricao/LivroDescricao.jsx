6dce775b2758ccda941aca7e24b3e6e212f1e635
laismullerrr <lais.muller@hotmail.com>
2023-10-21 14:12:57 -0300

    feat: criação da página de descrição do livro

--- src/pages/LivroDescricao/LivroDescricao.jsx
+++ src/pages/LivroDescricao/LivroDescricao.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import CapaLivro from '../../assets/livro-capa-teste.jpg'
 
 const LivroDescricao = () => {
   return (
-    <div>LIVRO DESCRIÇÃO</div>
+    <section className='container-descricao'>
+      <div className='container-flex'>
+        <div className='container-imagem-livro'>
+          <img src={CapaLivro} alt='capa do livro'/>
+        </div>
+
+        <div className='container-detalhes-livro'>
+          <h2>Nome do livro</h2>
+          <p>Autor do Livro</p>
+          <p>Descrição do Livro</p>
+          <p>Gênero</p>
+          <h3>R$39,90</h3>
+        </div>
+      </div>
+    </section>
   )
 }
 
