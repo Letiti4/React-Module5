16da3b799620bd8293854939030f1c529ffeb68c
Thzzao <thiago.santos1160@gmail.com>
2023-10-21 03:44:04 -0300

    feat: Criação de mais livros para teste

--- src/components/ListaProdutos/ListaProdutos.jsx
+++ src/components/ListaProdutos/ListaProdutos.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import '../ListaProdutos/listaProdutos.styles.css'
 import CardLivro from "../CardLivro/CardLivro";
+import BarraPesquisa from "../BarraPesquisa/BarraPesquisa";
 
 const ListaProdutos = () => {
-  return (
-    <div className="container-lista-produtos">
-      <div>
-        <h2>Alguns títulos que podem te interessar: </h2>
-        <div className="container-lista">
-          <CardLivro/>
-          <CardLivro/>
-          <CardLivro/>
-          <CardLivro/>
-        </div>
-      </div>
-    </div>
-  );
-};
+
+    // const [pesquisa, setPesquisa] = useState("") 
+
+    return (
+        <>
+            <div className="container-lista-produtos">
+                <div>
+                    <h2>Alguns títulos que podem te interessar: </h2>
+                    <BarraPesquisa />
+
+                    <div className="container-lista">
+                        <CardLivro titulo={'Alice no país das maravilhas'} autor={'Dona MAria'} preco={'R$39,90'} />
+                        <CardLivro titulo={'Titanic'} autor={'Thiago Souza'} preco={'R$59,90'} />
+                        <CardLivro titulo={'Pinóquio'} autor={'George Marlon'} preco={'R$55,90'} />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                        <CardLivro />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default ListaProdutos;
