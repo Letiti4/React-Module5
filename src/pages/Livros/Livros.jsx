4593a88be166a24c079f5e40526523b399d26649
Thzzao <thiago.santos1160@gmail.com>
2023-10-21 03:47:09 -0300

    feat: Chamada do header e da lista de livros

--- src/pages/Livros/Livros.jsx
+++ src/pages/Livros/Livros.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ListaProdutos from '../../components/ListaProdutos/ListaProdutos'
+import Header from '../../components/Header/Header'
+import BarraPesquisa from '../../components/BarraPesquisa/BarraPesquisa'
 
 const Livros = () => {
-  return (
-    <div>Livros</div>
-  )
+    return (
+        <>
+            <Header />
+            <ListaProdutos />
+        </>
+    )
 }
 
 export default Livros
