ef2187a67f43b02e82609c98060e7c8a698b909b
laismullerrr <lais.muller@hotmail.com>
2023-10-23 15:31:02 -0300

    fix: remoção da barra de pesquisa na lista de produtos

--- src/components/ListaProdutos/ListaProdutos.jsx
+++ src/components/ListaProdutos/ListaProdutos.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import '../ListaProdutos/listaProdutos.styles.css'
 import CardLivro from "../CardLivro/CardLivro";
-import BarraPesquisa from "../BarraPesquisa/BarraPesquisa";
+// import BarraPesquisa from "../BarraPesquisa/BarraPesquisa";
 
 const ListaProdutos = () => {
 
     // const [pesquisa, setPesquisa] = useState("") 
 
     return (
-        <>
+        
             <div className="container-lista-produtos">
                 <div>
-                    <h2>Alguns títulos que podem te interessar: </h2>
-                    <BarraPesquisa />
+                    <h2> Promoções: </h2>
+                    {/* <BarraPesquisa /> */}
 
                     <div className="container-lista">
                         <CardLivro titulo={'Alice no país das maravilhas'} autor={'Dona MAria'} preco={'R$39,90'} />
-                        <CardLivro titulo={'Titanic'} autor={'Thiago Souza'} preco={'R$59,90'} />
-                        <CardLivro titulo={'Pinóquio'} autor={'George Marlon'} preco={'R$55,90'} />
+                        <CardLivro titulo={'Titanic: Memórias de uma naufrágio'} autor={'Thiago Souza'} preco={'R$59,90'} />
+                        <CardLivro titulo={'Pinóquio (Edição de Bolso)'} autor={'George Marlon'} preco={'R$55,90'} />
+                        <CardLivro />
+                        {/* <CardLivro />
                         <CardLivro />
                         <CardLivro />
                         <CardLivro />
@@ -27,13 +29,11 @@ const ListaProdutos = () => {
                         <CardLivro />
                         <CardLivro />
                         <CardLivro />
-                        <CardLivro />
-                        <CardLivro />
-                        <CardLivro />
+                        <CardLivro /> */}
                     </div>
                 </div>
             </div>
-        </>
+        
     )
 }
 
