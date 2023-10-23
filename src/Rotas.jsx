152ddfef6703223bc3481df34b7418f9b4c76c21
laismullerrr <lais.muller@hotmail.com>
2023-10-23 14:01:37 -0300

    fix: alteração no caminho do import

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage"
 import Livros from "./pages/Livros/Livros";
-import LivroDescricao from "./pages/LivroDescricao/LivroDescricao";
+import DetalhesLivro from "./pages/DetalhesLivro/DetalhesLivro"
 import Cadastro from "./pages/Cadastro/Cadastro";
 import Login from "./pages/Login/Login";
 
@@ -14,7 +14,7 @@ const Rotas = () => {
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/livros" element={<Livros />} />
-                <Route path="/livro-descricao/:id" element={<LivroDescricao />} />
+                <Route path="/livro-descricao/:id" element={<DetalhesLivro />} />
             </Routes>
         </BrowserRouter>
     );
