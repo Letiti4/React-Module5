e3f7c25fdc3810abd70c4ec0cada5cb2ccb98392
laismullerrr <lais.muller@hotmail.com>
2023-10-22 22:47:05 -0300

    feat: adição do footer na pág Livros

--- src/pages/Livros/Livros.jsx
+++ src/pages/Livros/Livros.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ListaProdutos from '../../components/ListaProdutos/ListaProdutos'
 import Header from '../../components/Header/Header'
 import BarraPesquisa from '../../components/BarraPesquisa/BarraPesquisa'
+import Footer from '../../components/Footer/Footer'
 
 const Livros = () => {
     return (
         <>
             <Header />
             <ListaProdutos />
+            <Footer/>
         </>
     )
 }
