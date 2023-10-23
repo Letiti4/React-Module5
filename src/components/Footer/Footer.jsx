b5e3b81e6e00091b173fa893e7073666010de4ce
laismullerrr <lais.muller@hotmail.com>
2023-10-22 23:20:21 -0300

    feat: adição de redes sociais no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,45 +1,69 @@
 // import React from 'react'
-import './Footer.styles.css'
+import "./Footer.styles.css";
+import facebook from "../../assets/Logos/facebook.png";
+import email from "../../assets/Logos/email.png";
+import instagram from "../../assets/Logos/insta.png";
+import whatsapp from "../../assets/Logos/whatsapp.png";
 
 const Footer = () => {
     function msgBotao() {
-        alert("Mensagem enviada com sucesso!")
+        alert("Mensagem enviada com sucesso!");
     }
     return (
         <section className="footerCaixa">
-            
-                
+            <div className="infos-site">
                 <form className="formFooter">
                     <h2>Entre em contato conosco!</h2>
                     <div className="formGroup">
-                        <label htmlFor="nome" className="formLabel">Nome:</label>
+                        <label htmlFor="nome" className="formLabel">
+                            Nome:
+                        </label>
                         <input type="text" id="nome" className="formInput" />
                     </div>
                     <div className="formGroup">
-                        <label htmlFor="email" className="formLabel">Email:</label>
+                        <label htmlFor="email" className="formLabel">
+                            Email:
+                        </label>
                         <input type="email" id="email" className="formInput" />
                     </div>
                     <div className="formGroup">
-                        <label htmlFor="mensagem" className="formLabel">Mensagem:</label>
-                        <textarea className="formText" id="mensagem" ></textarea>
+                        <label htmlFor="mensagem" className="formLabel">
+                            Mensagem:
+                        </label>
+                        <textarea className="formText" id="mensagem"></textarea>
                     </div>
                     <div className="formGroup">
-                        <button className="formEnviar" onClick={msgBotao} >Enviar</button>
+                        <button className="formEnviar" onClick={msgBotao}>
+                            Enviar
+                        </button>
                     </div>
                 </form>
-                <div className="footerInfo">
-                    <ul>
-                        <li>R. Marechal Deodoro, 100 - Curitiba</li>
-                        <li>Telefone: (41) 9 9999-999  -  CNPJ: 15.302.489/0001-019</li>
-                    </ul>
-                    <p>Todos os direitos reservados &copy; 2023 Espaço das Letras. </p>
+                <div className="redes-sociais">
+                    <a href="" target="_blank">
+                        <img
+                            src={instagram}
+                            alt="icone do instagram"
+                        />
+                    </a>
+                    <a href="" target="_blank">
+                        <img src={whatsapp} alt="icone do whatsapp" />
+                    </a>
+                    <a href="" target="_blank">
+                        <img src={facebook} alt="icone do facebook" />
+                    </a>
                 </div>
-            
+            </div>
+            <div className="footerInfo">
+                <ul>
+                    <li>R. Marechal Deodoro, 100 - Curitiba</li>
+                    <li>Telefone: (41) 9 9999-999 - CNPJ: 15.302.489/0001-019</li>
+                </ul>
+                <p>Todos os direitos reservados &copy; 2023 Espaço das Letras. </p>
+            </div>
 
             {/* COLOCAR BICICLETONA E REDES SOCIAIS */}
-            
         </section>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
