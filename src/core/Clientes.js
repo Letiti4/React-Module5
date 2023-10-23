b5f02fc3a99281467f814bd1a0932199b36c6e18
laismullerrr <lais.muller@hotmail.com>
2023-10-23 20:14:41 -0300

    feat: adição do banco de dados

---
+++ src/core/Clientes.js
@@ -0,0 +1,37 @@
+export const clientes =[
+    {
+      "ID": 1,
+      "NOME": "Robson Ademir",
+      "EMAIL": "robinho123@gmail.com",
+      "TELEFONE": "41 99519-8777",
+      "ENDERECO": "Rua Trajetoria Triste, 6669 - Bairro: Depressão"
+    },
+    {
+      "ID": 2,
+      "NOME": "Michel Teló",
+      "EMAIL": "michael159telozin@hotmail.com",
+      "TELEFONE": "11 99878-5564",
+      "ENDERECO": "Rua Leonardo Sistemico, 523 - Bairro: Sitio Cercado"
+    },
+    {
+      "ID": 3,
+      "NOME": "Joelma Kalipiçon",
+      "EMAIL": "jojokali@yahoo.com",
+      "TELEFONE": "41 99356-1478",
+      "ENDERECO": "Rua Jose Arruda, 398 - Bairro: Solidão"
+    },
+    {
+      "ID": 4,
+      "NOME": "Léo Stronda",
+      "EMAIL": "leleostrondinha@yahoo.com",
+      "TELEFONE": "24 95366-9275",
+      "ENDERECO": "Rua Leoncio ovelha, 666 - Bairro: CIC Loko"
+    },
+    {
+      "ID": 5,
+      "NOME": "Silvio Santinho",
+      "EMAIL": "sil002@gmail.com",
+      "TELEFONE": "11 98654-2145",
+      "ENDERECO": "Rua Trejano Reis, 350 - Bairro: Centro"
+    }
+  ]
---
+++ src/core/Livros.js
@@ -0,0 +1,91 @@
+export const livros = [
+    
+  {
+      id: "1",
+      titulo: "Dom Quixote",
+      autor: "Miguel de Cervantes",
+      estado: "novo",
+      preco: "R$24,90",
+      capa: 'https://m.media-amazon.com/images/I/91PtC5UZNoL._AC_UF1000,1000_QL80_.jpg'
+      },
+      
+      {
+      id: "2",
+      titulo: "Cem Anos de Solidão",
+      autor: "Gabriel García Márquez",
+      estado: "usado",
+      preco: "R$19,99",
+      capa: 'https://m.media-amazon.com/images/I/81SQPrWU7SL._AC_UF1000,1000_QL80_.jpg'
+      },
+      
+      {
+      id: "3",
+      titulo: "1984",
+      autor: "George Orwell",
+      estado: "novo",
+      preco: "R$29,90",
+      capa: 'https://m.media-amazon.com/images/I/61HtBosDhwL._AC_UF1000,1000_QL80_.jpg'
+      },
+      
+      {
+      id: "4",
+      titulo: "O Sol é para Todos",
+      autor: "Harper Lee",
+      estado: "usado",
+      preco: "R$17,50",
+      capa: 'https://m.media-amazon.com/images/I/91WKPd60P4L._AC_UF1000,1000_QL80_.jpg'
+      },
+      
+      {
+      id: "5",
+      titulo: "O Grande Gatsby",
+      autor: "F. Scott Fitzgerald",
+      estado: "usado",
+      preco: "R$16,95",
+      capa: 'https://m.media-amazon.com/images/I/81xjr9TiTtL._AC_UF1000,1000_QL80_.jpg'
+      },
+      
+      {
+      id: "6",
+      titulo: "O Apanhador no Campo de Centeio",
+      autor: "J.D. Salinger",
+      estado: "usado",
+      preco: "R$12,90",
+      capa: 'https://m.media-amazon.com/images/I/5193mgpULoL.jpg'
+      },
+      
+      {
+      id: "7",
+      titulo: "Em Busca do Tempo Perdido",
+      autor: "Marcel Proust",
+      estado: "usado",
+      preco: "R$29,95",
+      capa: 'https://m.media-amazon.com/images/I/41p+HWPi1yL.jpg'
+      },
+      
+      {
+      id: "8",
+      titulo: "O Senhor dos Anéis",
+      autor: "J.R.R. Tolkien",
+      estado: "usado",
+      preco: "R$39,90",
+      capa: 'https://m.media-amazon.com/images/I/71ZLavBjpRL._AC_UF1000,1000_QL80_.jpg'
+      },
+      
+      {
+      id: "9",
+      titulo: "A Revolução dos Bichos",
+      autor: "George Orwell",
+      estado: "novo",
+      preco: "R$29,99",
+      capa: 'https://m.media-amazon.com/images/I/612QiXA+FyL._AC_UF1000,1000_QL80_.jpg'
+      },
+      {
+          id: "10",
+          titulo: "Alice no País das Maravilhas",
+          autor: "Roberto",
+          estado: "usado",
+          preco: "R$22,90",
+          capa: 'https://m.media-amazon.com/images/I/91hq-bopcvL._AC_UF1000,1000_QL80_.jpg'
+      },
+]
