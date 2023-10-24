27f0d38fa9775e820886f039b82a2cc1032dd667
laismullerrr <lais.muller@hotmail.com>
2023-10-24 16:11:33 -0300

    feat: adição de dados para livros em promoção

---
+++ src/core/LivrosPromo.js
@@ -0,0 +1,39 @@
+export const livrosPromo = [
+
+{id: "12",
+titulo: "O Pequeno Príncipe",
+autor: "Antoine de Saint-Exupéry",
+estado: "usado",
+preco: "R$ 15,90",
+capa: 'https://m.media-amazon.com/images/I/51nNwwVSclL._AC_UF1000,1000_QL80_.jpg'
+},
+
+{
+id: "13",
+titulo: "A Menina que Roubava Livros",
+autor: "Markus Zusak",
+estado: "usado",
+preco: "R$ 22,95",
+capa: 'https://m.media-amazon.com/images/I/61Fo3CitEGL._AC_UF1000,1000_QL80_.jpg'
+},
+
+{
+    id: "14",
+    titulo: "O Código Da Vinci",
+    autor: "Dan Brown",
+    estado: "novo",
+    preco: "R$ 35,75",
+    capa: 'https://m.media-amazon.com/images/I/710DjekSUkL._AC_UF1000,1000_QL80_.jpg'
+    },
+
+    {
+        id: "15",
+        titulo: "A Culpa é das Estrelas",
+        autor: "John Green",
+        estado: "usado",
+        preco: "R$ 18,75",
+        capa: 'https://m.media-amazon.com/images/I/51M9IbBqxCL._AC_UF1000,1000_QL80_.jpg'
+        }
+
+
+]
