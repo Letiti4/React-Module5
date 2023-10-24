9f28cc86f2a430c94ebd12be5460521af16ab8ab
laismullerrr <lais.muller@hotmail.com>
2023-10-24 16:14:36 -0300

    feat: adição de lógicas do cadastro e confirmação de senha

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 import './Cadastro.styles.css'
 import livrosLc from "../../assets/livrosLc.jpg"
 import Header from '../../components/Header/Header'
 import { useNavigate } from 'react-router-dom'
 import Footer from '../../components/Footer/Footer'
+import { postUsuario } from "../../services/api";
 
 const Cadastro = () => {
+
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [confirmaSenha, setConfirmaSenha] = useState('')
     const navigate = useNavigate()
-    const handleClick = () => {
-        navigate('/login')
+    const handleCadastro = async (e) => {
+        e.preventDefault()
+        
+        const body = {
+            nome,
+            email
+        }
+
+        if (senha === confirmaSenha) {
+            const resposta = await postUsuario(body, senha)
+            localStorage.setItem('id', resposta.data.id)
+            localStorage.setItem('nome', resposta.data.nome)
+            console.log(resposta)
+            console.log("Deu certo, redirecionando para login"); //adicionar um alert ou modal para dizer que o cadastro foi realizado
+        navigate('/login');
+
+        } else {
+            alert('as senhas precisam ser iguais')
+        }
     }
     return (
         <>
@@ -24,18 +47,29 @@ const Cadastro = () => {
                             <form>
                                 <div className="formGroup">
                                     <label htmlFor="">Usuário:</label>
-                                    <input type="usuario" />
+                                    <input type="usuario"
+                                        value={nome}
+                                        onChange={(e) => setNome(e.target.value)} />
                                 </div>
                                 <div className="formGroup">
                                     <label htmlFor="">Email:</label>
-                                    <input type="email" />
+                                    <input type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)} />
                                 </div>
                                 <div className="formGroup">
                                     <label htmlFor="">Senha:</label>
-                                    <input type="senha" />
+                                    <input type="senha" value={senha}
+                                        onChange={(e) => setSenha(e.target.value)} />
                                 </div>
                                 <div className="formGroup">
-                                    <button onClick={() => { handleClick }} className="formEnviar">Criar conta</button>
+                                    <label htmlFor="">Confirmação de Senha:</label>
+                                    <input type="senha" value={confirmaSenha}
+                                        onChange={(e) => setConfirmaSenha(e.target.value)} />
+                                </div>
+                                <div className="formGroup">
+                                    <button width="100%" onClick={handleCadastro}
+                                        className="formEnviar">Criar conta</button>
                                 </div>
                             </form>
                         </div>
