1d54f2762382f49af4f4cfda75919f698719473d
laismullerrr <lais.muller@hotmail.com>
2023-10-24 19:19:29 -0300

    feat: adição de lógicas do login

--- src/services/api.js
+++ src/services/api.js
@@ -18,3 +18,28 @@ export const getLivros = async () => {
     const resposta = await api.post('/usuarios', body, config)
     return resposta.data
   }
+  
+  export const loginUsuario = async (email, senha) => {
+    try {
+      const config = {
+        headers: {
+          'X-password': senha,
+        },
+      }
+  
+      const resposta = await api.post('/login', { email }, config)
+  
+      return resposta.data
+    } catch (error) {
+      if (error.response) {
+        return {
+          message: error.response.data.message,
+          success: error.response.data.success,
+        }
+      } else {
+        return {
+          message: 'erro inesperado',
+        }
+      }
+    }
+  }
