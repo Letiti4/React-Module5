cd8bd11f1fb30cbad674afdc2de2ef66a7e2ebfa
Thzzao <thiago.santos1160@gmail.com>
2023-10-24 17:01:29 -0300

    refactor: Removendo importação desnecessária e rodando o run build

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage"
 import Livros from "./pages/Livros/Livros";
