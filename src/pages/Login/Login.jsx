33d25522c59c3d031ddced96faa9bd27e4d4083d
laismullerrr <lais.muller@hotmail.com>
2023-10-24 19:20:00 -0300

    feat: adição de lógicas do login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,33 @@
 // import React from 'react'
 import { useNavigate } from "react-router-dom"
-import livrosLc from "../../assets/livrosLC.jpg"
+import React, { useState } from 'react'
+import livrosLc from "../../assets/livrosLc.jpg"
 import Header from "../../components/Header/Header"
 import Footer from "../../components/Footer/Footer"
+import { loginUsuario } from '../../services/api';
+
 
 const Login = () => {
+
     const navigate = useNavigate()
-    const handleClick = () => {
-        navigate('/livros')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [error, setError] = useState()
+
+    async function handleLogin(e) {
+        e.preventDefault()
+        const resposta = await loginUsuario(email, senha)
+
+        if (resposta.success) {
+            navigate('/livros')
+            localStorage.setItem('id', resposta.data.id)
+            localStorage.setItem('nome', resposta.data.nome)
+        } else {
+            setError(resposta.message)
+        }
+        console.log(resposta)
     }
+
     return (
         <>
             <Header />
@@ -23,14 +42,17 @@ const Login = () => {
                             <form>
                                 <div className="formGroup">
                                     <label htmlFor="">Email:</label>
-                                    <input type="email" />
+                                    <input type="email" value={email}
+                                        onChange={(e) => setEmail(e.target.value)} />
                                 </div>
                                 <div className="formGroup">
                                     <label htmlFor="">Senha:</label>
-                                    <input type="senha" />
+                                    <input type="senha" value={senha}
+                                        onChange={(e) => setSenha(e.target.value)} />
+                                    {error && <p style={{ color: 'red' }}>{error}</p>}
                                 </div>
                                 <div className="formGroup">
-                                    <button onClick={handleClick} className="formEnviar">Entrar</button >
+                                    <button onClick={handleLogin} className="formEnviar">Entrar</button >
                                 </div>
                             </form>
                         </div>
