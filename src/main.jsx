9257a0d5738251f536ce214bf3d353d1759db4db
Thzzao <thiago.santos1160@gmail.com>
2023-10-25 15:27:05 -0300

    feat: Importação do react toastify em todo o projeto

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import '../src/styleGlobal.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Rotas from './Rotas'
+import { Flip, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ToastContainer
+      position="top-center" theme="dark" transition={Flip} style={{ width: "auto" }} />
     <Rotas />
   </React.StrictMode>,
 )
