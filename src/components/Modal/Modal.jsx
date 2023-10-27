33d3d077b5b7a468610abfbb9f79e21b61ea2dd5
Thzzao <thiago.santos1160@gmail.com>
2023-10-27 16:58:58 -0300

    refactor: Arrumando as funções dos botões

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import '../Modal/Modal.styles.css'
 
-const Modal = ({ open, fechaModal }) => {
+const Modal = ({ open, fechaModal, handleExcluirConta }) => {
 
     const handleClose = (e) => {
         e.target.id === "modal" && fechaModal();
@@ -16,8 +16,8 @@ const Modal = ({ open, fechaModal }) => {
                             <input id='confirmacao' type='password' placeholder='Digite sua senha para confirmar'></input>
                         </section>
                         <div className='botoes'>
-                            <button className='btnEnviar' onClick={handleClose}>Excluir</button>
-                            <button className='btnEnviar' onClick={handleClose}>Cancelar</button>
+                            <button className='btnEnviar' onClick={handleExcluirConta}>Excluir</button>
+                            <button className='btnEnviar' id="modal" onClick={handleClose}>Cancelar</button>
                         </div>
                     </div>
                 </div>
