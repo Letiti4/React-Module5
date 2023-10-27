227996954bebc5bce9525216875b1f36f89c3727
Thzzao <thiago.santos1160@gmail.com>
2023-10-27 16:12:34 -0300

    feat: Criação do modal para excluir a conta

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import '../Modal/Modal.styles.css'
+
+const Modal = ({ open, fechaModal }) => {
+
+    const handleClose = (e) => {
+        e.target.id === "modal" && fechaModal();
+    };
+    return (
+        <>
+            {open && (
+                <div className="modal" id="modal" onClick={handleClose} >
+                    <div className="contentModal">
+                        <section className="header">
+                            <p>Deseja realmente apagar a sua conta?</p>
+                            <input id='confirmacao' type='password' placeholder='Digite sua senha para confirmar'></input>
+                        </section>
+                        <div className='botoes'>
+                            <button className='btnEnviar' onClick={handleClose}>Excluir</button>
+                            <button className='btnEnviar' onClick={handleClose}>Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Modal;
+
