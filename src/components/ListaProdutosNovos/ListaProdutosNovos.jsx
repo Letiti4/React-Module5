b9f7e659bff1b702ee211868589798efd7e133f7
laismullerrr <lais.muller@hotmail.com>
2023-10-27 21:17:42 -0300

    feat: ligação dos elementos do card com a api

--- src/components/ListaProdutosNovos/ListaProdutosNovos.jsx
+++ src/components/ListaProdutosNovos/ListaProdutosNovos.jsx
@@ -1,26 +1,64 @@
 import '../ListaProdutosNovos/listaProdutosNovos.styles.css'
 import { livrosPromo } from '../../core/LivrosPromo';
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 import CardLivro from '../CardLivro/CardLivro';
+import { getLivros } from '../../services/api';
 
-const ListaProdutosNovos = () => {
+// const ListaProdutosNovos = () => {
 
-    const [pesquisa] = useState("")
+//     const [pesquisa] = useState("")
 
-    return (
-        <div className="container-lista-produtos">
-            <div>
-                <h1> Recém-chegados: </h1>
+//     const urlApi = "http://localhost:3000/livros"
+//     const [livros, setLivros] = useState([])
 
-                <div className="container-lista">
-                    {livrosPromo.filter((livro) => livro.titulo.toLowerCase().includes(pesquisa.toLowerCase())).map(livro => 
-                    <CardLivro key={livro.id} titulo={livro.titulo} autor={livro.autor} preco={livro.preco} capa={livro.capa} link={'/livro-descricao/12'} />
-                    )}
+//     const getLivros = async() => {
+//         try {
+//             const resposta = await axios.get(urlApi)
+//             const data = resposta.data
+//             setLivros(data)
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+
+    // useEffect(() => {
+    //     getLivros()
+    // }, [])
+    const ListaProdutosNovos = () => {
+        const [livros, setLivros] = useState([]);
+    
+        useEffect(() => {
+            getLivros()
+                .then((data) => {
+                    setLivros(data);
+                })
+                .catch((error) => {
+                    console.error('Erro ao buscar livros:', error);
+                });
+        }, []);
+    
+        return (
+            <div className="container-lista-produtos">
+                <div>
+                    <h1> Recém-chegados: </h1>
+                    <div className="container-lista">
+                        {livros.map((livro) => (
+                            <div key={livro._id}>
+                                <CardLivro
+                                    titulo={livro.titulo}
+                                    autor={livro.autor}
+                                    preco={livro.preco}
+                                    capa={livro.capa}
+                                    link={livro.link}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-
             </div>
-        </div>
-    )
-}
+        );
+    }
 
 export default ListaProdutosNovos;
